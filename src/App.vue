95fbb37812a0d05824275c5bd3060dd2160240fd
Barbara Liau <bj.liau@gmail.com>
2017-11-09 10:53:56 -0700

    Initial commit with basic concept

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app :dark="true">
+    <v-toolbar fixed :prominent="true" app>
+      <!-- <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+      </v-btn> -->
+      <!-- <v-btn icon @click.stop="clipped = !clipped">
+        <v-icon>web</v-icon>
+      </v-btn> -->
+      <v-toolbar-title class="logo">
+        <img src="/static/koa_logo_white.png"></img>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <main>
+      <v-content>
+        <v-container text-xs-center red--text text--darken-4>
+          <h2>Knockout 36 Challenge</h2>
+        </v-container>
+        <Rank></Rank>
+        <v-layout row wrap justify-center>
+            36 days left!
+          </v-layout>
+      </v-content>
+    </main>    
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  import Rank from '@/components/Rank'
+  
+  export default {
+    components: { Rank },
+
+    data () {
+      return {
+        fixed: false,
+        title: 'KOA Kingdom Challenge - Knockout 36'
+      }
+    }
+  }
+
+</script>
+
+<style lang="stylus" scoped>
+  .logo img
+    width 50%
+    margin-top 5px
+</style>
+
---
+++ src/components/Rank.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-container fluid grid-list-md>
+    <v-layout row wrap justify-center>
+      <v-flex
+        v-for="(item, index) in list" :key="index"
+        xl1 lg2 md2 sm4 xs6
+      >
+
+        <v-progress-circular
+          v-bind:size="150"
+          v-bind:width="15"
+          v-bind:rotate="-90"
+          v-bind:value="item.classesAttended * 2.78"
+          color="teal"
+        >
+          <h4 class="teal--text text--lighten-2 text-xs-center addClass"
+            @click="addClass"
+          >
+            {{ item.classesAttended }}
+          </h4>
+          {{ item.firstName }} {{ item.lastName }}
+        </v-progress-circular>        
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Rank',
+
+  data () {
+    return {
+      value: 36,
+      rawList: [{
+        classesAttended: 36,
+        firstName: 'Jill',
+        lastName: 'Smith',
+        paid: true
+      }, {
+        classesAttended: 5,
+        firstName: 'Jill',
+        lastName: 'Smith',
+        paid: true
+      }, {
+        classesAttended: 8,
+        firstName: 'Jill',
+        lastName: 'Smith',
+        paid: true
+      }, {
+        classesAttended: 5,
+        firstName: 'Jill',
+        lastName: 'Smith',
+        paid: true
+      }, {
+        classesAttended: 10,
+        firstName: 'Jill',
+        lastName: 'Smith',
+        paid: true
+      }]
+    }
+  },
+
+  computed: {
+    list: function () {
+      return this.rawList
+        .filter((i) => i.paid === true)
+        .sort((a, b) => b.classesAttended - a.classesAttended)
+    }
+  },
+
+  methods: {
+    addClass () {
+      console.log('add')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .addClass
+    &:hover
+      cursor crosshair
+</style>
