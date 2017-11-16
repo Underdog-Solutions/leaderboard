d75a872abf76b1a12898837456c6aa78d9c2ff9f
Barbara Liau <bj.liau@gmail.com>
2017-11-16 11:23:53 -0700

    Add firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import Firebase from 'firebase'
+
+let config = {
+  apiKey: 'AIzaSyAQI1NWgGm8OhN7eDQSsilfSVpVh6iNnjQ',
+  authDomain: 'koa-leaderboard.firebaseapp.com',
+  databaseURL: 'https://koa-leaderboard.firebaseio.com',
+  projectId: 'koa-leaderboard',
+  storageBucket: '',
+  messagingSenderId: '826393811885'
+}
+
+let app = Firebase.initializeApp(config)
+let db = app.database()
+let pplRef = db.ref('ppl')
+
+export default {
+  db,
+  pplRef
+}
+
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
 import Vue from 'vue'
+import VueFire from 'vuefire'
 
 import {
   Vuetify,
@@ -22,6 +23,7 @@ import './stylus/main.styl'
 import App from './App'
 import router from './router'
 
+Vue.use(VueFire)
 Vue.use(Vuetify, {
   components: {
     VApp,
